9aa3cbfc6e9b8e04cd7124730bac383011c388de
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2022-12-27 05:41:16 +0100

    initial commit

---
+++ movie_app_swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  movie_app_swift
+//
+//  Created by cindy-dev on 22/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ movie_app_swift/movie_app_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_app_swiftApp.swift
+//  movie_app_swift
+//
+//  Created by cindy-dev on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct movie_app_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
