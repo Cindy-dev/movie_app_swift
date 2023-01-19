d00a5a466e87e757c5960cf2d6f02396859cf4c9
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-01-19 12:27:44 +0100

    fixes

--- movie_app_swift/views/ContentView.swift
+++ movie_app_swift/views/ContentView.swift
@@ -10,36 +10,51 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Rectangle()
-                .frame(width: 200, height: 500)
-                .foregroundColor(.green)
-                .overlay(Text("Hello")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .padding()
-                         
-                )
-            Spacer()
-            Rectangle()
-                .frame(width: 200, height: 100)
-                .foregroundColor(.green)
-                .overlay(
-                    VStack{
-                        Image("download")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .clipped()
-                        Text("Avenger")
-                            .foregroundColor(.blue)
-                            .font(.largeTitle)
-                            .padding()
-                    }
-                
-                    
-                )
-            Spacer()
-            
-        }
+                ZStack {
+                    Image("download")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 9 * 0.7, height: 50 * 0.05)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white, lineWidth: 2)
+                        )
+                        .padding(.all, 10)
+                        .offset(x: 10, y: 8)
+                }
+            }
+//        VStack {
+//            Rectangle()
+//                .frame(width: 200, height: 500)
+//                .foregroundColor(.green)
+//                .overlay(Text("Hello")
+//                    .foregroundColor(.white)
+//                    .font(.largeTitle)
+//                    .padding()
+//
+//                )
+//            Spacer()
+//            Rectangle()
+//                .frame(width: 200, height: 100)
+//                .foregroundColor(.green)
+//                .overlay(
+//                    VStack{
+//                        Image("download")
+//                            .resizable()
+//                            .aspectRatio(contentMode: .fill)
+//                            .clipped()
+//                        Text("Avenger")
+//                            .foregroundColor(.blue)
+//                            .font(.largeTitle)
+//                            .padding()
+//                    }
+//
+//
+//                )
+//            Spacer()
+//
+//        }
         
         
     }
